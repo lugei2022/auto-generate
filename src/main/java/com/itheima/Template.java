1208aa1bd197c809a657665ea97e6209ee6981d5
lugei <3320454018@qq.com>
2023-03-19 11:57:00 +0800

    添加一个模板文件

---
+++ src/main/java/com/itheima/Template.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class Template {
+	public static void main(String[] args) {
+		// main psvm
+		// System.out.println("Template.main"); soutm
+		// System.out.println("args = " + Arrays.toString(args)); soutp
+		// System.out.println("args = " + args); soutv
+	}
+}
